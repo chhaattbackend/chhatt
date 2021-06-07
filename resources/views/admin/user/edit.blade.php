60671897f1e2f7e2d77217bc98604c57ed854b18
yasirpervez07 <yasirpervez07@gmail.com>
2021-06-07 15:18:53 +0500

    stage

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -50,7 +50,7 @@
                                 <div class="form-group row">
                                     <label for="inputEmail3" class="col-sm-2 col-form-label">Firebase</label>
                                     <div class="col-sm-6">
-                                        <input value="{{ $user->firebase_id }}" required type="text" name="firebase_id"
+                                        <input value="{{ $user->firebase_id }}"  type="text" name="firebase_id"
                                             class="form-control">
                                     </div>
 
